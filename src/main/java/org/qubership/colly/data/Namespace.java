faf6726ac6b6c4737f141ee9edc50a2aecae249a
Roman Romanov <ormigus@gmail.com>
2025-02-21 18:42:46 +0300

    Initial Prototype to read environments, namespaces and clusters

---
+++ src/main/java/org/qubership/colly/data/Namespace.java
@@ -0,0 +1,4 @@
+package org.qubership.colly.data;
+
+public record Namespace(String name, String uid, String envName) {
+}
---
+++ src/test/java/org/qubership/EnvironmentsResourceIT.java
@@ -0,0 +1,8 @@
+package org.qubership;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class EnvironmentsResourceIT extends EnvironmentsResourceTest {
+    // Execute the same tests but in packaged mode.
+}
